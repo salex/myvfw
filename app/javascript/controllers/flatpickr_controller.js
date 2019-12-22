fc991c50edb9205fd885119b4b0e7f53c5cc5a81
Steve Alex <salex@mac.com>
2019-12-21 19:09:19 -0600

    fix picker error his insead of thhis

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -9,7 +9,7 @@ export default class extends Flatpickr {
   initialize() {
      //global options
     console.log("got a picker")
-    his.config = {
+    this.config = {
       altInput: true,
       altFormat: "F j, Y",
       dateFormat: "Y-m-d"
