4d5ea42344960f1988e877eefd029bdef4bbc52f
Steve Alex <salex@mac.com>
2020-05-31 08:05:45 -0500

    deploy production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,58 @@
+set :user,            'rails'
+
+role :app, %w{rails@104.248.4.230}
+role :web, %w{rails@104.248.4.230}
+role :db,  %w{rails@104.248.4.230}
+
+
+# Don't change these unless you know what you're doing
+set :pty,             true
+# set :use_sudo,        false
+set :stage,           :production
+# set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  # after  :finishing,    :restart
+
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
